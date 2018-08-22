e50477577d60534ee3edd77666f51169202cfc48
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:33:35 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,11 @@
 class Hash
   def keys_of(*arguments)
-   array_holder = []
+   
    self.each do |key, value|
      if arguments.include?(value)
        array_holder << key
      end
    end
   end
-  array_holder
+ 
 end
